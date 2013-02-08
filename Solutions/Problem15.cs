12c8ceaa7b36b004863e50e39d9cbafa5ba2006c
Bora Akgun <boraakgn@gmail.com>
2013-02-08 18:07:27 +0200

    problem 15

--- Solutions/Problem15.cs
+++ Solutions/Problem15.cs
@@ -1,10 +1,21 @@
-﻿namespace Solutions
+﻿using System.Numerics;
+namespace Solutions
 {
     public static class Problem15
     {
-        public static void Solve()
+        private static BigInteger Factorial(int n)
         {
-            
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+
+        public static object Solve()
+        {
+            BigInteger result = Factorial(40) / (Factorial(20) * Factorial(20));
+            return result;
         }
     }
 }
